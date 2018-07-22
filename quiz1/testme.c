37024b0fc562b6cd0c056af08319f44b1e3fa80d
Wenda Zhou <zhouw@os1.engr.oregonstate.edu>
2018-07-22 11:03:16 -0700

    quiz 1

---
+++ quiz1/testme.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+
+char inputChar()
+{
+    // TODO: rewrite this function
+    int n=rand()%94+32; // generate from space to } 
+    char c=(char) n;
+
+   return c;
+}
+
+char *inputString()
+{
+    // TODO: rewrite this function
+    char str[6];
+    int i;
+    
+    for (i=0; i<5; i++){
+      int  n=rand()%16+101; // generate from e to t
+      char c=(char) n;   
+      str[i]=c;
+      }
+     str[5]='\0';
+     char * result=str;   
+   
+    /* str[0]='r';
+     str[1]='e';
+     str[2]='s';
+     str[3]='e';
+     str[4]='t';str[5]='\0';
+     char * result=str;
+    */
+
+
+
+    return result;
+}
+
+void testme()
+{
+  int tcCount = 0;
+  char *s;
+  char c;
+  int state = 0;
+  while (1)
+  {
+    tcCount++;
+    c = inputChar();
+    s = inputString();
+    printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+
+    if (c == '[' && state == 0) state = 1;
+    if (c == '(' && state == 1) state = 2;
+    if (c == '{' && state == 2) state = 3;
+    if (c == ' '&& state == 3) state = 4;
+    if (c == 'a' && state == 4) state = 5;
+    if (c == 'x' && state == 5) state = 6;
+    if (c == '}' && state == 6) state = 7;
+    if (c == ')' && state == 7) state = 8;
+    if (c == ']' && state == 8) state = 9;
+    if (s[0] == 'r' && s[1] == 'e'
+       && s[2] == 's' && s[3] == 'e'
+       && s[4] == 't' && s[5] == '\0'
+       && state == 9)
+    {
+      printf("error ");
+      exit(200);
+    }
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    testme();
+    return 0;
+}
